36c775a465b612937c66a823ea15cbe090116425
Oladeji Sanyaolu <oladejisanyaolu@gmail.com>
2022-10-27 22:14:22 +0100

    getting ready to test multiplayer

--- src/World/NetworkSetup.cs
+++ src/World/NetworkSetup.cs
@@ -5,6 +5,9 @@ using System;
 
 public partial class NetworkSetup : Control
 {
+	// Private variables
+	// Nodes
+	private MultiplayerAPI _multiplayer;
 	private Button _createServer;
 	private Button _joinServer;
 
@@ -23,17 +26,26 @@ public partial class NetworkSetup : Control
 		var error = multiplayer.JoinServer(multiplayer.IpAddress);
 	}
 
+	// Network signals
+	private void _onMultiplayerConnectedToServer()
+	{
+		GD.Print("Connected to Server Successfully");
+	}
 	/*
 		Godot methods
 	*/
 	public override void _Ready()
 	{
 		// Get the nodes from their NodePaths
+		_multiplayer = GetTree().GetMultiplayer();
 		_createServer = (Button) GetNode<Button>("CreateServer");
 		_joinServer = (Button) GetNode<Button>("JoinServer");
 
 		// Connect the signals
 		_createServer.Pressed += () => _onCreateServerPressed();
 		_joinServer.Pressed += () => _onJoinServerPressed();
+		
+		// Network signals
+		_multiplayer.ConnectedToServer += () => _onMultiplayerConnectedToServer();
 	}
 }
